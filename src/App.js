e33343d7345a8b7940f86b82c46ccbb2320c2511
2023317MaxDyson <maxdyson@gmail.com>
2025-04-06 14:37:50 -0600

    Added the course name to the page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h2> Max Dyson </h2>
+    <h2> Max Dyson <br/> <br/> Course Name: TECH2102:Enterprise Computing-25JANMNFS2 </h2>
     </div>
   );
 }
